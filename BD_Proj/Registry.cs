22d6dc5c8593dbe3c86ce7faeca780f6e7c324cd
Bartek <bartek0794@gmail.com>
2017-09-12 00:12:50 +0200

    poprawa rejestracji - nie można stworzyć pustego konta

--- BD_Proj/Registry.cs
+++ BD_Proj/Registry.cs
@@ -23,36 +23,42 @@ namespace BD_Proj
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             String scoreId = "1", userId = "1";
 
-            String queryScoreId = "SELECT TOP 1 scoreId FROM SCORES ORDER BY scoreId DESC";
-            SqlCommand commandScoreId = new SqlCommand(queryScoreId, conn);
-
-            SqlDataReader reader = commandScoreId.ExecuteReader();
-            while (reader.Read())
+            if (loginTextBox.Text.ToString() != "" && passwordTextBox.Text.ToString() != "")
             {
-                scoreId = reader[0].ToString();
-            }
-            reader.Close();
+                String queryScoreId = "SELECT TOP 1 scoreId FROM SCORES ORDER BY scoreId DESC";
+                SqlCommand commandScoreId = new SqlCommand(queryScoreId, conn);
 
-            String queryUserId = "SELECT TOP 1 userId FROM USERS ORDER BY scoreId DESC";
-            SqlCommand commandUserId = new SqlCommand(queryUserId, conn);
-           
-            SqlDataReader readerUserid = commandUserId.ExecuteReader();
-            while (readerUserid.Read())
-            {
-                userId = readerUserid[0].ToString();
-            }
-            readerUserid.Close();
-            
-            scoreId = (int.Parse(scoreId) + 1).ToString();
-            userId = (int.Parse(userId) + 1).ToString();
+                SqlDataReader reader = commandScoreId.ExecuteReader();
+                while (reader.Read())
+                {
+                    scoreId = reader[0].ToString();
+                }
+                reader.Close();
 
-            String queryScores = "INSERT INTO SCORES VALUES ("+ scoreId + ",0,0);" + "INSERT INTO USERS VALUES (" + userId +","+ "'" + loginTextBox.Text.ToString() + "'" + "," + "'" + passwordTextBox.Text.ToString() + "'" + ","  + 2 + "," + scoreId +")";
-            SqlCommand command1 = new SqlCommand(queryScores, conn);
-            command1.ExecuteNonQuery();
-            this.Close();
+                String queryUserId = "SELECT TOP 1 userId FROM USERS ORDER BY scoreId DESC";
+                SqlCommand commandUserId = new SqlCommand(queryUserId, conn);
+
+                SqlDataReader readerUserid = commandUserId.ExecuteReader();
+                while (readerUserid.Read())
+                {
+                    userId = readerUserid[0].ToString();
+                }
+                readerUserid.Close();
+
+                scoreId = (int.Parse(scoreId) + 1).ToString();
+                userId = (int.Parse(userId) + 1).ToString();
+                String queryScores = "INSERT INTO SCORES VALUES (" + scoreId + ",0,0);" + "INSERT INTO USERS VALUES (" + userId + "," + "'" + loginTextBox.Text.ToString() + "'" + "," + "'" + passwordTextBox.Text.ToString() + "'" + "," + 2 + "," + scoreId + ")";
+                SqlCommand command1 = new SqlCommand(queryScores, conn);
+                command1.ExecuteNonQuery();
+
+                this.Close();
+            }
+            else {
+                MessageBox.Show("Login lub hasło puste!");
+            }
+
           }
 
         private void closeConnection(object sender, FormClosedEventArgs e)
